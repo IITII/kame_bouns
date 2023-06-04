a37c43fda5aaae6474a1f3ec9a021abeb9efcbd3
IITII <ccmejx@gmail.com>
2023-06-04 09:09:22 +0000

    bouns-=6w

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const config = require('./config/config'),
     path = require('path'),
     cloudscraper = require('cloudscraper')
 
-const url = 'https://kamept.com/mybonus.php'
+const url = 'https://kamept.com/medal.php'
 const file = path.resolve(__dirname, './cache.json')
 
 async function getDom() {
@@ -21,17 +21,17 @@ async function getDom() {
             Cookie: config.cookies,
         }
     })
-    // return await axios.get(url, {
-    //     responseType: 'document',
-    //     headers: {
-    //         'referer': url,
-    //         Host: new URL(url).host,
-    //         Connection: 'keep-alive',
-    //         Cookie: config.cookies,
-    //     },
-    // })
+        // return await axios.get(url, {
+        //     responseType: 'document',
+        //     headers: {
+        //         'referer': url,
+        //         Host: new URL(url).host,
+        //         Connection: 'keep-alive',
+        //         Cookie: config.cookies,
+        //     },
+        // })
         // .then(res => {return load(res?.data)})
-        .then(res => {return load(res)})
+        .then(res => { return load(res) })
         .then($ => {
             if ($.text().includes('未登录')) {
                 throw new Error('Cookies 过期')
@@ -64,29 +64,37 @@ async function tgNotify(text) {
         text,
     }
     axios.post(`https://api.telegram.org/bot${config.tg.token}/sendMessage`, params)
-    .then(_ => console.log(`sent: ${text}`))
-    .catch(e => console.log(`tgNotify: ${e.message}`))
+        .then(_ => console.log(`sent: ${text}`))
+        .catch(e => console.log(`tgNotify: ${e.message}`))
 }
 
 async function task() {
     console.log(`start task`);
-    await getDom().then(async $ => {
-        let bouns = $("#outer table:contains('上传量') tr").filter(function (i, el) {
+    await getDom().then(async jQuery => {
+        let bouns = jQuery("#outer table:contains('库存') tr").filter(function (i, el) {
             // this === el
-            return $(this).find("td[align='left'] img").length > 0
+            return jQuery(this).find("td img[class='preview']").length > 0
+        }).filter(function (i, el) {
+            return jQuery(this).find("td h1:contains('勋章')").length === 0
         }).map((i, el) => {
-            return {
-                title: $(el).find('h1').text(),
-                desc: $(el).find("div[style*=padding]").text(),
-                img: $(el).find('img').map((i, el) => el.attribs.src).toArray()[0],
-                price: $(el).find('td:nth-child(4)').text()
-            }
-        }).toArray().map(_ => ({ ..._, img: node_url.resolve(url, _.img) }))
+            let title, desc, img, price, canBuyTime, vaildityPeriod
+            // img = jQuery(el).find("img[class='preview']").map((i, el) => el.currentSrc).toArray()[0]
+            img = jQuery(el).find("img[class='preview']").map((i, el) => el.attribs.src).toArray()[0]
+            img = node_url.resolve(url, img)
+            title = jQuery(el).find('h1').text().trim()
+            desc = jQuery(el).find("td:nth-child(2)").text()
+            desc = (desc || '').replace(title, '').trim()
+            canBuyTime = jQuery(el).find('td:nth-child(3)').text()
+            vaildityPeriod = jQuery(el).find('td:nth-child(4)').text()
+            price = jQuery(el).find('td:nth-child(6)').text()
+            
+            return { img, title, desc, canBuyTime,vaildityPeriod, price }
+        })
         let oldJson = loadJson()
         let newBouns = bouns.filter(b => !oldJson.some(o => o.title === b.title))
         for (const n of newBouns) {
             console.log(bouns);
-            let msg = `#名称 _${n.title}_\n#价格 ${n.price}\n${n.desc} ${n.img}`
+            let msg = `#名称 _${n.title}_\n#价格 ${n.price}\n#可购买时间 ${n.canBuyTime}\n#购买后有效期(天) ${n.vaildityPeriod}\n${n.desc} ${n.img}`
             await tgNotify(msg)
             await sleep(1000)
         }
@@ -99,9 +107,10 @@ async function task() {
 }
 
 async function main() {
-    setInterval(async () => {
-        await task()
-    }, config.interval)
+    // setInterval(async () => {
+    //     await task()
+    // }, config.interval)
+    await task()
 }
 
 main()
